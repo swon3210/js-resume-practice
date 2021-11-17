6404e8755ec38e4800955edf37f9b4dba9a78142
swon3210 <swon3210@gmail.com>
2021-11-18 01:43:21 +0900

    fix: indicators template 을 join으로 합치지 않는 문제 해결

--- template.js
+++ template.js
@@ -2,5 +2,5 @@ export const getIndicatorsTemplate = (count) => {
   return [...Array(count)].map(
     (_, index) =>
       `<a href="#section-${index}" class="indicator-wrapper"><span class="indicator"></span></a>`
-  );
+  ).join("");
 };
